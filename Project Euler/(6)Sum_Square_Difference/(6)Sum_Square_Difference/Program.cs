caaec090b8a89bb2be84bc04c91819d0f7154dfc
Bryce McGee <bryce.mcgee@gmail.com>
2013-05-08 18:05:09 -0700

    Problem 6 Complete

---
+++ Project Euler/(6)Sum_Square_Difference/(6)Sum_Square_Difference/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Sum_Square_Difference
+{
+    class Program
+    {
+        //Find the difference between the sum of the squares of the first one hundred
+        //natural numbers and the square of the sum.
+
+        static void Main(string[] args)
+        {
+
+
+            int naturalNumber = 1;
+            double numbersSquared;
+            double numberSquaredTotal = 0;
+            double SquareOfSums = 0;
+            int total;
+
+            while (naturalNumber <= 100)
+            {
+
+                numbersSquared = Math.Pow(naturalNumber, 2);
+
+                numberSquaredTotal = numberSquaredTotal + numbersSquared;
+
+                SquareOfSums += naturalNumber; 
+
+                naturalNumber++;
+            }
+
+            SquareOfSums = Math.Pow(SquareOfSums, 2);
+
+            total = (int)SquareOfSums - (int)numberSquaredTotal;
+            Console.WriteLine("Square of Sums: " + SquareOfSums);
+            Console.WriteLine("Sum of Squares: " + numberSquaredTotal);
+            Console.WriteLine(SquareOfSums + "-" + numberSquaredTotal);
+            Console.WriteLine("Answer: " + total);
+
+
+            
+
+
+
+
+        }
+    }
+}
