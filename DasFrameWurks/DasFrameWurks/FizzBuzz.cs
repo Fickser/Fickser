24736abe91c76729b30310a24dc0455db688f0a7
Fickser <bryce.mcgee@gmail.com>
2013-12-14 14:08:30 -0800

    Implemented FizzBuzz with tests.

---
+++ DasFrameWurks/DasFrameWurks/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DasFrameWurks
+{
+  public class FizzBuzz : IFizzBuzz
+    {
+      public List<string> GetFizzBuzz(List<int> Values)
+      {
+          List<string> ReturnSequence = new List<string>();
+
+          foreach(int i in Values)
+          {
+              if (i%3 == 0 && i%5 !=0)
+              {
+                  string whatever = i.ToString() + "Fizz";
+                  ReturnSequence.Add(whatever);
+              } 
+              else if(i%5 == 0 && i%3 !=0)
+              {
+                  string whatever = i.ToString() + "Buzz";
+                  ReturnSequence.Add(whatever);
+              }
+              else if(i%5 == 0 && i%3 == 0)
+              {
+                  string whatever = i.ToString() + "FizzBuzz";
+                  ReturnSequence.Add(whatever);
+              }
+              else
+              {
+                  string whatever = i.ToString();
+                  ReturnSequence.Add(whatever);
+              }
+          }
+
+          return ReturnSequence;
+      }
+    }
+}
---
+++ DasFrameWurks/UnitTestProject1/FizzBuzzTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DasFrameWurks;
+
+namespace DasFrameWurks.test //Arrange, Act, Assert
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void GivenNumbersDivisibleByThreeButNotFiveExpectFizz()
+        {
+            // Arrange
+            List<int> StartingSequence = new List<int>() { 3, 6, 9 };
+            List<string> ExpectedSequence = new List<string>() { "3Fizz", "6Fizz", "9Fizz" };
+            IFizzBuzz FizzBuzz = new FizzBuzz();
+
+            // Act
+            List<string> ActualSequence = FizzBuzz.GetFizzBuzz(StartingSequence);
+
+            //Assert
+            CollectionAssert.AreEqual(ExpectedSequence, ActualSequence);
+        }
+
+        [TestMethod]
+        public void GivenNumbersDivisibleByFiveButNotThreeExpectBuzz()
+        {
+            // Arrange
+            List<int> StartingSequence = new List<int>() { 5, 10, 20 };
+            List<string> ExpectedSequence = new List<string>() { "5Buzz", "10Buzz", "20Buzz" };
+            IFizzBuzz FizzBuzz = new FizzBuzz();
+
+            // Act
+            List<string> ActualSequence = FizzBuzz.GetFizzBuzz(StartingSequence);
+
+            //Assert
+            CollectionAssert.AreEqual(ExpectedSequence, ActualSequence);
+
+        }
+
+        [TestMethod]
+        public void GivenNumbersDivisibleByFiveAndThreeExpectFizzBuzz()
+        {
+            // Arrange
+            List<int> StartingSequence = new List<int>() { 15, 30, 45 };
+            List<string> ExpectedSequence = new List<string>() { "15FizzBuzz", "30FizzBuzz", "45FizzBuzz" };
+            IFizzBuzz FizzBuzz = new FizzBuzz();
+
+            // Act
+            List<string> ActualSequence = FizzBuzz.GetFizzBuzz(StartingSequence);
+
+            //Assert
+            CollectionAssert.AreEqual(ExpectedSequence, ActualSequence);
+        }
+
+        [TestMethod]
+        public void GivenNumbersNotDivisibleByFiveAndThreeExpectNumber()
+        {
+            // Arrange
+            List<int> StartingSequence = new List<int>() { 7, 14, 22 };
+            List<string> ExpectedSequence = new List<string>() { "7", "14", "22" };
+            IFizzBuzz FizzBuzz = new FizzBuzz();
+
+            // Act
+            List<string> ActualSequence = FizzBuzz.GetFizzBuzz(StartingSequence);
+
+            //Assert
+            CollectionAssert.AreEqual(ExpectedSequence, ActualSequence);
+        }
+
+        [TestMethod]
+        public void GivenTehGauntletExpectTehGauntlet()
+        {
+            // Arrange
+            List<int> StartingSequence = new List<int>() { 3, 25, 107, 30 };
+            List<string> ExpectedSequence = new List<string>() { "3Fizz", "25Buzz", "107", "30FizzBuzz" };
+            IFizzBuzz FizzBuzz = new FizzBuzz();
+
+            // Act
+            List<string> ActualSequence = FizzBuzz.GetFizzBuzz(StartingSequence);
+
+            //Assert
+            CollectionAssert.AreEqual(ExpectedSequence, ActualSequence);
+        }
+    }
+}
